fc414c1e54b458b12cb1a6be8a667e6d78006743
valery <valery.fremaux@gmail.com>
2018-03-25 10:12:38 +0200

    fix travis

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ $plugin->version  = 2017082401;   // The (date) version of this plugin.
 $plugin->requires = 2016120500;   // Requires this Moodle version.
 $plugin->component = 'local_ent_installer';
 $plugin->release = '3.2.0 (Build 2017082401)';
-// $plugin->dependencies = array('local_vmoodle' => '2013020800'); // Not mandatory.
+$plugin->dependencies = array('local_vflibs' => '2016081100');
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.2.0005';
